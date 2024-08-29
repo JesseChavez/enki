402f2b4946526afb501454acf9e86eb3f72807ce
Jesse Chavez <jesse.chavez.r@gmail.com>
2024-08-29 21:11:28 +1000

    Set sslmode to disable for postgres

--- database/postgres.go
+++ database/postgres.go
@@ -5,8 +5,8 @@ import (
 )
 
 func UrlForPostgres(conf Config) string {
-	// postgres://username:password@localhost:5432/my_db
-	format := "%s://%s:%s@%s:%s/%s"
+	// postgres://username:password@localhost:5432/my_db?sslmode=disable
+	format := "%s://%s:%s@%s:%s/%s?sslmode=disable"
 
 	url := fmt.Sprintf(format, conf.Adapter, conf.Username, conf.Password, conf.Host, conf.Port, conf.Database)
 
