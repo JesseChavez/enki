ac7455c5cb1b5d80e4b56ebebe899086421c8250
Jesse Chavez <jesse.chavez.r@gmail.com>
2026-05-09 17:20:51 +0900

    Encode password when building db url

--- database/postgres.go
+++ database/postgres.go
@@ -2,13 +2,16 @@ package database
 
 import (
 	"fmt"
+	"net/url"
 )
 
 func UrlForPostgres(conf Config) (string, string) {
 	// postgres://username:password@localhost:5432/my_db?sslmode=disable
 	format := "%s://%s:%s@%s:%s/%s?sslmode=%s"
 
-	realUrl := fmt.Sprintf(format, conf.Adapter, conf.Username, conf.Password, conf.Host, conf.Port, conf.Database, conf.Sslmode)
+	password := url.QueryEscape(conf.Password)
+
+	realUrl := fmt.Sprintf(format, conf.Adapter, conf.Username, password, conf.Host, conf.Port, conf.Database, conf.Sslmode)
 
 	fakeUrl := fmt.Sprintf(format, conf.Adapter, "[∗∗∗∗]", "[∗∗∗∗]", conf.Host, conf.Port, conf.Database, conf.Sslmode)
 
